75c6bb780cd3bfb56022cfeb1670f804c6d913c8
Tayyaba <tayyabamunawar14@gmail.com>
2026-05-10 01:18:47 +0200

    quick  updates

--- lab1/run_network.py
+++ lab1/run_network.py
@@ -16,97 +16,53 @@ class NetworkTopo(Topo):
         # Hosts
         ############################################################
 
-        h1 = self.addHost(
-            'h1',
-            ip='10.0.1.2/24',
-            defaultRoute='via 10.0.1.1'
-        )
-
-        h2 = self.addHost(
-            'h2',
-            ip='10.0.1.3/24',
-            defaultRoute='via 10.0.1.1'
-        )
-
-        ser = self.addHost(
-            'ser',
-            ip='10.0.2.2/24',
-            defaultRoute='via 10.0.2.1'
-        )
-
-        ext = self.addHost(
-            'ext',
-            ip='192.168.1.123/24',
-            defaultRoute='via 192.168.1.1'
-        )
+        h1 = self.addHost('h1', ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
+        h2 = self.addHost('h2', ip='10.0.1.3/24', defaultRoute='via 10.0.1.1')
+        ser = self.addHost('ser', ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
+        ext = self.addHost('ext', ip='192.168.1.123/24', defaultRoute='via 192.168.1.1')
 
         ############################################################
         # Switches
         ############################################################
 
-        s1 = self.addSwitch('s1', dpid='1')
-        s2 = self.addSwitch('s2', dpid='2')
-        s3 = self.addSwitch('s3', dpid='3')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
 
         ############################################################
-        # Link options
+        # Links
         ############################################################
 
-        link_opts = dict(
-            bw=15,
-            delay='10ms'
-        )
+        link_opts = dict(bw=15, delay='10ms')
 
-        ############################################################
-        # Host links
-        ############################################################
+        # -------------------------
+        # Host <-> Switch links
+        # -------------------------
 
-        self.addLink(h1, s1, **link_opts)
-        self.addLink(h2, s1, **link_opts)
+        self.addLink(h1, s1, port1=0, port2=1, **link_opts)
+        self.addLink(h2, s1, port1=0, port2=2, **link_opts)
 
-        self.addLink(ser, s2, **link_opts)
+        self.addLink(ser, s2, port1=0, port2=1, **link_opts)
 
-        ############################################################
-        # Router links with FIXED PORT NUMBERS
-        ############################################################
+        self.addLink(ext, s3, port1=0, port2=3, **link_opts)
 
-        self.addLink(
-            s1,
-            s3,
-            port2=1,
-            **link_opts
-        )
+        # -------------------------
+        # Router core links (FIXED)
+        # -------------------------
 
-        self.addLink(
-            s2,
-            s3,
-            port2=2,
-            **link_opts
-        )
-
-        self.addLink(
-            ext,
-            s3,
-            port2=3,
-            **link_opts
-        )
+        self.addLink(s1, s3, port1=3, port2=1, **link_opts)
+        self.addLink(s2, s3, port1=2, port2=2, **link_opts)
 
 
 def run():
 
-    topo = NetworkTopo()
-
     net = Mininet(
-        topo=topo,
+        topo=NetworkTopo(),
         switch=OVSKernelSwitch,
         link=TCLink,
         controller=None,
         autoSetMacs=True
     )
-
-    ############################################################
-    # Remote controller
-    ############################################################
 
     net.addController(
         'c1',
@@ -115,19 +71,11 @@ def run():
         port=6653
     )
 
-    ############################################################
-    # Start network
-    ############################################################
-
     net.start()
-
     CLI(net)
-
     net.stop()
 
 
 if __name__ == '__main__':
-
     setLogLevel('info')
-
     run()
