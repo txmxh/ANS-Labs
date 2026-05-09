15d81e95d6a3eeafbca9b37153ea52f5c8d8ceb9
Tayyaba <tayyabamunawar14@gmail.com>
2026-05-10 00:13:51 +0200

    updated both files

--- lab1/ans_controller.py
+++ lab1/ans_controller.py
@@ -1,106 +1,263 @@
+# Fixed `ans_controller.py`
+
+```python
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet, ethernet, arp, ipv4, icmp
 
+
 class LearningSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(LearningSwitch, self).__init__(*args, **kwargs)
-        # Router configurations from manual
-        self.port_to_own_mac = {1: "00:00:00:00:01:01", 2: "00:00:00:00:01:02", 3: "00:00:00:00:01:03"}
-        self.port_to_own_ip = {1: "10.0.1.1", 2: "10.0.2.1", 3: "192.168.1.1"}
-        # Per-switch MAC table
+
+        # Router interface MAC addresses
+        self.port_to_own_mac = {
+            1: "00:00:00:00:01:01",
+            2: "00:00:00:00:01:02",
+            3: "00:00:00:00:01:03"
+        }
+
+        # Router interface IP addresses
+        self.port_to_own_ip = {
+            1: "10.0.1.1",
+            2: "10.0.2.1",
+            3: "192.168.1.1"
+        }
+
+        # MAC learning tables for switches
         self.mac_to_port = {}
-        # Router's ARP cache to remember host MACs
-        self.arp_table = {"10.0.1.2": "00:00:00:00:00:01", "10.0.1.3": "00:00:00:00:00:02", 
-                          "10.0.2.2": "00:00:00:00:00:03", "192.168.1.123": "00:00:00:00:00:04"}
+
+        # ARP cache learned dynamically
+        self.arp_table = {}
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+
+        # Send unknown packets to controller
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
+        actions = [
+            parser.OFPActionOutput(
+                ofproto.OFPP_CONTROLLER,
+                ofproto.OFPCML_NO_BUFFER
+            )
+        ]
+
         self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
+
+        inst = [
+            parser.OFPInstructionActions(
+                ofproto.OFPIT_APPLY_ACTIONS,
+                actions
+            )
+        ]
+
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=priority,
+            match=match,
+            instructions=inst
+        )
+
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
+
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
         dpid = datapath.id
+        in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
-        if not eth: return
-        
-        dst_mac, src_mac = eth.dst, eth.src
+
+        if eth is None:
+            return
+
+        # Ignore LLDP
+        if eth.ethertype == 0x88cc:
+            return
+
+        src = eth.src
+        dst = eth.dst
+
         arp_pkt = pkt.get_protocol(arp.arp)
         ipv4_pkt = pkt.get_protocol(ipv4.ipv4)
+        icmp_pkt = pkt.get_protocol(icmp.icmp)
 
-        if dpid == 3:  # Router Logic (s3)
+        ################################################################
+        # ROUTER LOGIC (s3)
+        ################################################################
+        if dpid == 3:
+
+            # Learn ARP dynamically
+            if arp_pkt:
+                self.arp_table[arp_pkt.src_ip] = arp_pkt.src_mac
+
+            ############################################################
+            # Handle ARP requests to router gateways
+            ############################################################
             if arp_pkt and arp_pkt.opcode == arp.ARP_REQUEST:
+
                 target_ip = arp_pkt.dst_ip
-                if target_ip == self.port_to_own_ip.get(in_port):
+
+                if target_ip in self.port_to_own_ip.values():
+
+                    # Only allow hosts to ping their OWN gateway
+                    if target_ip != self.port_to_own_ip[in_port]:
+                        return
+
                     router_mac = self.port_to_own_mac[in_port]
-                    reply = packet.Packet()
-                    reply.add_protocol(ethernet.ethernet(dst=src_mac, src=router_mac, ethertype=eth.ethertype))
-                    reply.add_protocol(arp.arp(opcode=arp.ARP_REPLY, src_mac=router_mac, src_ip=target_ip, 
-                                               dst_mac=src_mac, dst_ip=arp_pkt.src_ip))
-                    reply.serialize()
-                    out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER, 
-                                              in_port=ofproto.OFPP_CONTROLLER, actions=[parser.OFPActionOutput(in_port)], data=reply.data)
+
+                    arp_reply = packet.Packet()
+
+                    arp_reply.add_protocol(
+                        ethernet.ethernet(
+                            ethertype=0x0806,
+                            dst=src,
+                            src=router_mac
+                        )
+                    )
+
+                    arp_reply.add_protocol(
+                        arp.arp(
+                            opcode=arp.ARP_REPLY,
+                            src_mac=router_mac,
+                            src_ip=target_ip,
+                            dst_mac=arp_pkt.src_mac,
+                            dst_ip=arp_pkt.src_ip
+                        )
+                    )
+
+                    arp_reply.serialize()
+
+                    actions = [parser.OFPActionOutput(in_port)]
+
+                    out = parser.OFPPacketOut(
+                        datapath=datapath,
+                        buffer_id=ofproto.OFP_NO_BUFFER,
+                        in_port=ofproto.OFPP_CONTROLLER,
+                        actions=actions,
+                        data=arp_reply.data
+                    )
+
                     datapath.send_msg(out)
 
-            elif ipv4_pkt:
-                # Security: Block external discovery pings
-                if ipv4_pkt.src.startswith('192.168.1') and pkt.get_protocol(icmp.icmp): return
-                # Security: Block ext <-> ser connections
-                if (ipv4_pkt.src == '192.168.1.123' and ipv4_pkt.dst == '10.0.2.2') or \
-                   (ipv4_pkt.src == '10.0.2.2' and ipv4_pkt.dst == '192.168.1.123'): return
-                # Security: Only allow pinging own gateway
-                if ipv4_pkt.dst in self.port_to_own_ip.values() and ipv4_pkt.dst != self.port_to_own_ip.get(in_port): return
+                return
 
-                # Routing Logic
+            ############################################################
+            # IPv4 routing
+            ############################################################
+            if ipv4_pkt:
+
+                src_ip = ipv4_pkt.src
+                dst_ip = ipv4_pkt.dst
+
+                # Block ICMP from external host to internal hosts
+                if src_ip.startswith("192.168.1") and icmp_pkt:
+                    return
+
+                # Block TCP/UDP between ext and ser
+                if (
+                    (src_ip == "192.168.1.123" and dst_ip == "10.0.2.2")
+                    or
+                    (src_ip == "10.0.2.2" and dst_ip == "192.168.1.123")
+                ):
+                    return
+
+                # Determine output port
                 out_port = None
-                if ipv4_pkt.dst.startswith('10.0.1'): out_port = 1
-                elif ipv4_pkt.dst.startswith('10.0.2'): out_port = 2
-                elif ipv4_pkt.dst.startswith('192.168.1'): out_port = 3
 
-                if out_port:
-                    host_mac = self.arp_table.get(ipv4_pkt.dst)
-                    if host_mac:
-                        actions = [parser.OFPActionDecNwTtl(), 
-                                   parser.OFPActionSetField(eth_src=self.port_to_own_mac[out_port]), 
-                                   parser.OFPActionSetField(eth_dst=host_mac),
-                                   parser.OFPActionOutput(out_port)]
-                        match = parser.OFPMatch(eth_type=0x0800, ipv4_dst=ipv4_pkt.dst)
-                        self.add_flow(datapath, 1, match, actions)
-                        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, 
-                                                  actions=actions, data=msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None)
-                        datapath.send_msg(out)
+                if dst_ip.startswith("10.0.1."):
+                    out_port = 1
 
-        else:  # Learning Switch Logic (s1/s2)
+                elif dst_ip.startswith("10.0.2."):
+                    out_port = 2
+
+                elif dst_ip.startswith("192.168.1."):
+                    out_port = 3
+
+                if out_port is None:
+                    return
+
+                # Need destination MAC
+                if dst_ip not in self.arp_table:
+                    return
+
+                dst_mac = self.arp_table[dst_ip]
+                src_mac = self.port_to_own_mac[out_port]
+
+                actions = [
+                    parser.OFPActionSetField(eth_src=src_mac),
+                    parser.OFPActionSetField(eth_dst=dst_mac),
+                    parser.OFPActionDecNwTtl(),
+                    parser.OFPActionOutput(out_port)
+                ]
+
+                match = parser.OFPMatch(
+                    eth_type=0x0800,
+                    ipv4_dst=dst_ip
+                )
+
+                self.add_flow(datapath, 10, match, actions)
+
+                out = parser.OFPPacketOut(
+                    datapath=datapath,
+                    buffer_id=msg.buffer_id,
+                    in_port=in_port,
+                    actions=actions,
+                    data=(msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None)
+                )
+
+                datapath.send_msg(out)
+
+        ################################################################
+        # SWITCH LOGIC (s1 and s2)
+        ################################################################
+        else:
+
             self.mac_to_port.setdefault(dpid, {})
-            self.mac_to_port[dpid][src_mac] = in_port
-            out_port = self.mac_to_port[dpid].get(dst_mac, ofproto.OFPP_FLOOD)
+
+            # Learn source MAC
+            self.mac_to_port[dpid][src] = in_port
+
+            # Lookup destination MAC
+            if dst in self.mac_to_port[dpid]:
+                out_port = self.mac_to_port[dpid][dst]
+            else:
+                out_port = ofproto.OFPP_FLOOD
+
             actions = [parser.OFPActionOutput(out_port)]
+
+            # Install flow only for known destinations
             if out_port != ofproto.OFPP_FLOOD:
-                match = parser.OFPMatch(in_port=in_port, eth_dst=dst_mac)
+
+                match = parser.OFPMatch(
+                    in_port=in_port,
+                    eth_dst=dst
+                )
+
                 self.add_flow(datapath, 1, match, actions)
-            out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions, 
-                                      data=msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None)
+
+            out = parser.OFPPacketOut(
+                datapath=datapath,
+                buffer_id=msg.buffer_id,
+                in_port=in_port,
+                actions=actions,
+                data=(msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None)
+            )
+
             datapath.send_msg(out)
--- lab1/run_network.py
+++ lab1/run_network.py
@@ -1,97 +1,67 @@
-"""
- Copyright (c) 2026 Computer Networks Group @ UPB
-
- Permission is hereby granted, free of charge, to any person obtaining a copy of
- this software and associated documentation files (the "Software"), to deal in
- the Software without restriction, including without limitation the rights to
- use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- the Software, and to permit persons to whom the Software is furnished to do so,
- subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- """
-
 #!/bin/env python3
-
-#!/bin/env python3
-
-
 
 from mininet.topo import Topo
-
 from mininet.net import Mininet
-
 from mininet.node import RemoteController, OVSKernelSwitch
-
 from mininet.link import TCLink
-
 from mininet.cli import CLI
-
 from mininet.log import setLogLevel
-
-
-
 
 
 class NetworkTopo(Topo):
 
+    def build(self):
 
+        # Hosts
+        h1 = self.addHost(
+            'h1',
+            ip='10.0.1.2/24',
+            defaultRoute='via 10.0.1.1'
+        )
 
-    def __init__(self):
+        h2 = self.addHost(
+            'h2',
+            ip='10.0.1.3/24',
+            defaultRoute='via 10.0.1.1'
+        )
 
+        ser = self.addHost(
+            'ser',
+            ip='10.0.2.2/24',
+            defaultRoute='via 10.0.2.1'
+        )
 
+        ext = self.addHost(
+            'ext',
+            ip='192.168.1.123/24',
+            defaultRoute='via 192.168.1.1'
+        )
 
-        Topo.__init__(self)
-
-
-
-        # create the hosts
-
-        h1  = self.addHost('h1',  ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
-
-        h2  = self.addHost('h2',  ip='10.0.1.3/24', defaultRoute='via 10.0.1.1')
-
-        ser = self.addHost('ser', ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
-
-        ext = self.addHost('ext', ip='192.168.1.123/24', defaultRoute='via 192.168.1.1')
-
-
-
-        # create switches 
-
+        # Switches
         s1 = self.addSwitch('s1')
-
         s2 = self.addSwitch('s2')
-
-        s3 = self.addSwitch('s3')  # This is the router
-
-
-
-        # create connections         
+        s3 = self.addSwitch('s3')
 
         link_opts = dict(bw=15, delay='10ms')
 
-        self.addLink(h1,  s1, **link_opts)
+        # Internal LAN
+        self.addLink(h1, s1, **link_opts)
+        self.addLink(h2, s1, **link_opts)
 
-        self.addLink(h2,  s1, **link_opts)
-
+        # Server LAN
         self.addLink(ser, s2, **link_opts)
 
-        self.addLink(ext, s3, **link_opts)
+        # IMPORTANT:
+        # Explicit router interface numbering
 
-        self.addLink(s1,  s3, **link_opts)
+        # s3-eth1 -> internal network 10.0.1.0/24
+        self.addLink(s1, s3, port2=1, **link_opts)
 
-        self.addLink(s2,  s3, **link_opts)
+        # s3-eth2 -> server network 10.0.2.0/24
+        self.addLink(s2, s3, port2=2, **link_opts)
 
-
+        # s3-eth3 -> external network 192.168.1.0/24
+        self.addLink(ext, s3, port2=3, **link_opts)
 
 
 
@@ -99,23 +69,20 @@ def run():
 
     topo = NetworkTopo()
 
-    net = Mininet(topo=topo,
-
-                  switch=OVSKernelSwitch,
-
-                  link=TCLink,
-
-                  controller=None)
+    net = Mininet(
+        topo=topo,
+        switch=OVSKernelSwitch,
+        link=TCLink,
+        controller=None,
+        autoSetMacs=True
+    )
 
     net.addController(
-
-        'c1', 
-
-        controller=RemoteController, 
-
-        ip="127.0.0.1", 
-
-        port=6653)
+        'c1',
+        controller=RemoteController,
+        ip='127.0.0.1',
+        port=6653
+    )
 
     net.start()
 
