518d3c3a807e4800154235703d214cf9d722003f
Tayyaba <tayyabamunawar14@gmail.com>
2026-05-09 22:54:34 +0200

    updated file structure

--- lab1/run_network.py
+++ lab1/run_network.py
@@ -21,37 +21,112 @@
 
 #!/bin/env python3
 
+#!/bin/env python3
+
+
+
 from mininet.topo import Topo
+
 from mininet.net import Mininet
+
 from mininet.node import RemoteController, OVSKernelSwitch
+
 from mininet.link import TCLink
+
 from mininet.cli import CLI
+
 from mininet.log import setLogLevel
+
+
+
 
 
 class NetworkTopo(Topo):
 
-    def __init__(self):
 
-        Topo.__init__(self)
 
-        # Build the specified network topology here
+    def _init_(self):
+
+
+
+        Topo._init_(self)
+
+
+
+        # create the hosts
+
+        h1  = self.addHost('h1',  ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
+
+        h2  = self.addHost('h2',  ip='10.0.1.3/24', defaultRoute='via 10.0.1.1')
+
+        ser = self.addHost('ser', ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
+
+        ext = self.addHost('ext', ip='192.168.1.123/24', defaultRoute='via 192.168.1.1')
+
+
+
+        # create switches 
+
+        s1 = self.addSwitch('s1')
+
+        s2 = self.addSwitch('s2')
+
+        s3 = self.addSwitch('s3')  # This is the router
+
+
+
+        # create connections         
+
+        link_opts = dict(bw=15, delay='10ms')
+
+        self.addLink(h1,  s1, **link_opts)
+
+        self.addLink(h2,  s1, **link_opts)
+
+        self.addLink(ser, s2, **link_opts)
+
+        self.addLink(ext, s3, **link_opts)
+
+        self.addLink(s1,  s3, **link_opts)
+
+        self.addLink(s2,  s3, **link_opts)
+
+
+
+
 
 def run():
+
     topo = NetworkTopo()
+
     net = Mininet(topo=topo,
+
                   switch=OVSKernelSwitch,
+
                   link=TCLink,
+
                   controller=None)
+
     net.addController(
+
         'c1', 
+
         controller=RemoteController, 
+
         ip="127.0.0.1", 
+
         port=6653)
+
     net.start()
+
     CLI(net)
+
     net.stop()
 
-if __name__ == '__main__':
+
+
+if _name_ == '_main_':
+
     setLogLevel('info')
+
     run()
