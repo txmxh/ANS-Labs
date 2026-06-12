f89563278b791c0c622163a878591d2204f5cee6
Tayyaba <tayyabamunawar14@gmail.com>
2026-06-12 02:48:05 +0200

    fixed fat-tree

--- lab2/fat-tree.py
+++ lab2/fat-tree.py
@@ -75,7 +75,7 @@ class FattreeNet(Topo):
 def make_mininet_instance(graph_topo):
 
     net_topo = FattreeNet(graph_topo)
-    net = Mininet(topo=net_topo, controller=None, autoSetMacs=True)
+    net = Mininet(topo=net_topo, controller=None, autoSetMacs=True, link=TCLink)
     net.addController('c0', controller=RemoteController,
                       ip="127.0.0.1", port=6653)
     return net
